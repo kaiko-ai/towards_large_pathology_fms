caaf75cecade2e0325f98f22100d150a4830a772
ioangatop <johngatop@gmail.com>
2024-07-22 14:07:14 +0200

    Add support for multi-level feature extraction

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,6 @@
 """Pathology FM model hub from kaiko.ai."""
 
 import timm
-from timm.models import vision_transformer
 import torch
 from torch import nn
 
@@ -13,141 +12,126 @@ RELEASE_TAG = "0.0.1"
 """The release tag to fetch the weights from."""
 
 
-def vits16(*, pretrained: bool = True, dynamic_img_size: bool = True) -> nn.Module:
+def vits16(dynamic_img_size: bool = True, out_indices: int | List[int] | None = None) -> nn.Module:
     """Initializes the vision transformer ViTS-16 pathology FM by kaiko.ai.
 
     Args:
-        pretrained: Whether to load the pretrained model weights.
         dynamic_img_size: Whether to allow the interpolation embedding
             to be interpolated at `forward()` time when image grid changes
             from original.
+        out_indices: Weather and which multi-level patch embeddings to return.
 
     Returns:
         The torch ViTS-16 based foundation model.
     """
-    model = timm.create_model(
+    return timm.create_model(
         model_name="vit_small_patch16_224",
         dynamic_img_size=dynamic_img_size,
-        num_classes=0,
-        pretrained=False,
+        pretrained_cfg={
+            "url": f"https://github.com/kaiko-ai/towards_large_pathology_fms/releases/download/{RELEASE_TAG}/vits16.pth",
+            "num_classes": 0
+        },
+        pretrained=True,
+        out_indices=out_indices,
+        features_only=out_indices is not None,
     )
-    if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(
-            url=f"https://github.com/kaiko-ai/towards_large_pathology_fms/releases/download/{RELEASE_TAG}/vits16.pth",
-            map_location="cpu",
-        )
-        model.load_state_dict(state_dict, strict=True)
-
-    return model
 
 
-def vits8(*, pretrained: bool = True, dynamic_img_size: bool = True) -> nn.Module:
+def vits8(dynamic_img_size: bool = True, out_indices: int | List[int] | None = None) -> nn.Module:
     """Initializes the vision transformer ViTS-8 pathology FM by kaiko.ai.
 
     Args:
-        pretrained: Whether to load the pretrained model weights.
         dynamic_img_size: Whether to allow the interpolation embedding
             to be interpolated at `forward()` time when image grid changes
             from original.
+        out_indices: Weather and which multi-level patch embeddings to return.
 
     Returns:
         The torch ViTS-8 based foundation model.
     """
-    model = timm.create_model(
+    return timm.create_model(
         model_name="vit_small_patch8_224",
         dynamic_img_size=dynamic_img_size,
-        num_classes=0,
-        pretrained=False,
+        pretrained_cfg={
+            "url": f"https://github.com/kaiko-ai/towards_large_pathology_fms/releases/download/{RELEASE_TAG}/vits8.pth",
+            "num_classes": 0
+        },
+        pretrained=True,
+        out_indices=out_indices,
+        features_only=out_indices is not None,
     )
-    if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(
-            url=f"https://github.com/kaiko-ai/towards_large_pathology_fms/releases/download/{RELEASE_TAG}/vits8.pth",
-            map_location="cpu",
-        )
-        model.load_state_dict(state_dict, strict=True)
-
-    return model
 
 
-def vitb16(*, pretrained: bool = True, dynamic_img_size: bool = True) -> nn.Module:
+def vitb16(dynamic_img_size: bool = True, out_indices: int | List[int] | None = None) -> nn.Module:
     """Initializes the vision transformer ViTB-16 pathology FM by kaiko.ai.
 
     Args:
-        pretrained: Whether to load the pretrained model weights.
         dynamic_img_size: Whether to allow the interpolation embedding
             to be interpolated at `forward()` time when image grid changes
             from original.
+        out_indices: Weather and which multi-level patch embeddings to return.
 
     Returns:
         The torch ViTB-16 based foundation model.
     """
-    model = timm.create_model(
+    return timm.create_model(
         model_name="vit_base_patch16_224",
         dynamic_img_size=dynamic_img_size,
-        num_classes=0,
-        pretrained=False,
+        pretrained_cfg={
+            "url": f"https://github.com/kaiko-ai/towards_large_pathology_fms/releases/download/{RELEASE_TAG}/vitb16.pth",
+            "num_classes": 0
+        },
+        pretrained=True,
+        out_indices=out_indices,
+        features_only=out_indices is not None,
     )
-    if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(
-            url=f"https://github.com/kaiko-ai/towards_large_pathology_fms/releases/download/{RELEASE_TAG}/vitb16.pth",
-            map_location="cpu",
-        )
-        model.load_state_dict(state_dict, strict=True)
-
-    return model
 
 
-def vitb8(*, pretrained: bool = True, dynamic_img_size: bool = True) -> nn.Module:
+def vitb8(dynamic_img_size: bool = True, out_indices: int | List[int] | None = None) -> nn.Module:
     """Initializes the vision transformer ViTB-8 pathology FM by kaiko.ai.
 
     Args:
-        pretrained: Whether to load the pretrained model weights.
         dynamic_img_size: Whether to allow the interpolation embedding
             to be interpolated at `forward()` time when image grid changes
             from original.
+        out_indices: Weather and which multi-level patch embeddings to return.
 
     Returns:
         The torch ViTB-8 based foundation model.
     """
-    model = timm.create_model(
+    return timm.create_model(
         model_name="vit_base_patch8_224",
         dynamic_img_size=dynamic_img_size,
-        num_classes=0,
-        pretrained=False,
+        pretrained_cfg={
+            "url": f"https://github.com/kaiko-ai/towards_large_pathology_fms/releases/download/{RELEASE_TAG}/vitb8.pth",
+            "num_classes": 0
+        },
+        pretrained=True,
+        out_indices=out_indices,
+        features_only=out_indices is not None,
     )
-    if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(
-            url=f"https://github.com/kaiko-ai/towards_large_pathology_fms/releases/download/{RELEASE_TAG}/vitb8.pth",
-            map_location="cpu",
-        )
-        model.load_state_dict(state_dict, strict=True)
-
-    return model
 
 
-def vitl14(*, pretrained: bool = True, dynamic_img_size: bool = True) -> nn.Module:
+def vitl14(dynamic_img_size: bool = True, out_indices: int | List[int] | None = None) -> nn.Module:
     """Initializes the vision transformer ViTL-14 pathology FM by kaiko.ai.
 
     Args:
-        pretrained: Whether to load the pretrained model weights.
         dynamic_img_size: Whether to allow the interpolation embedding
             to be interpolated at `forward()` time when image grid changes
             from original.
+        out_indices: Weather and which multi-level patch embeddings to return.
 
     Returns:
         The torch ViTL-14 based foundation model.
     """
-    model = timm.create_model(
+    return timm.create_model(
         model_name="vit_large_patch14_reg4_dinov2",
+        pretrained_cfg={
+            "url": f"https://github.com/kaiko-ai/towards_large_pathology_fms/releases/download/{RELEASE_TAG}/vitl14.pth",
+            "num_classes": 0,
+        },
+        pretrained=True,
+        out_indices=out_indices,
         dynamic_img_size=dynamic_img_size,
-        pretrained=False,
+        features_only=out_indices is not None,
     )
-    if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(
-            url=f"https://github.com/kaiko-ai/towards_large_pathology_fms/releases/download/{RELEASE_TAG}/vitl14.pth",
-            map_location="cpu",
-        )
-        adapted_state_dict = vision_transformer._convert_dinov2(state_dict, model)
-        model.load_state_dict(adapted_state_dict, strict=True)
-
-    return model
